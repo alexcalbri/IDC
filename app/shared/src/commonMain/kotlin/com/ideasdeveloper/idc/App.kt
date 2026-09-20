e443880a60e8dd39cdab207a7e03406be7503f14
Brian Calero <bcalero@ideasdeveloper.com>
2026-09-20 00:43:12 -0600

    login

--- app/shared/src/commonMain/kotlin/com/ideasdeveloper/idc/App.kt
+++ app/shared/src/commonMain/kotlin/com/ideasdeveloper/idc/App.kt
@@ -1,64 +1,31 @@
-// app/shared/src/commonMain/kotlin/com/ideasdeveloper/idc/App.kt
 package com.ideasdeveloper.idc
-
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
-
-import idc.app.shared.generated.resources.Res
-import idc.app.shared.generated.resources.compose_multiplatform
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ideasdeveloper.idc.ui.screens.DashboardScreen
 import com.ideasdeveloper.idc.ui.screens.LoginScreen
-
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        var showLogin by remember { mutableStateOf(false) }
+        val navController = rememberNavController()
 
-        if (showLogin) {
-            LoginScreen(
-                onLoginSuccess = { showLogin = false }
-            )
-        } else {
-            Column(
-                modifier = Modifier
-                    .background(MaterialTheme.colorScheme.primaryContainer)
-                    .safeContentPadding()
-                    .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Button(onClick = { showContent = !showContent }) {
-                    Text("Click me!")
-                }
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Button(onClick = { showLogin = true }) {
-                    Text("Login")
-                }
-
-                AnimatedVisibility(showContent) {
-                    val greeting = remember { Greeting().greet() }
-                    Column(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                    ) {
-                        Image(painterResource(Res.drawable.compose_multiplatform), null)
-                        Text("Compose: $greeting")
-                    }
-                }
+        NavHost(
+            navController = navController,
+            startDestination = "login"
+        ) {
+            composable("login") {
+                LoginScreen(
+                    onLoginSuccess = { navController.navigate("dashboard") },
+                )
+            }
+            composable("dashboard") {
+                DashboardScreen(
+                    onNavigateTo = { navController.navigate("dashboard") },
+                )
             }
         }
     }
